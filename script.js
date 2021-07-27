df9cd0a3d698b13c026d13c23d3c6f2724b820fb
Stephen Kurshner <skurshner@gmail.com>
2021-07-27 11:09:16 -0700

    added basic math functions and operator function

---
+++ script.js
@@ -0,0 +1,32 @@
+// basic calculator functions
+
+// add
+add = (a,b) => a + b;
+
+// subtract
+subtract = (a,b) => a - b;
+
+// multiply
+multiply = (a,b) => a * b;
+
+// divide 
+divide = (a,b) => a / b;
+
+// operator function 
+function operate (operator,a,b) {
+    if (operator === "+") {
+        return add(a,b);
+    } else if (operator === "-") {
+        return subtract(a,b);
+    } else if (operator === "*") {
+        return multiply(a,b);
+    } else if (operator === "/") {
+        return divide(a,b);
+    }
+        
+}
+    
+
+    
+
+console.log(operate("/",10,2));
