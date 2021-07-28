30f73983ba1371bce2c02595dfa3b65b63933405
Stephen Kurshner <skurshner@gmail.com>
2021-07-27 21:03:00 -0700

    continuous math functions work, truncated decimal places to 4, cannot divide by 0, conditions of assignment met

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let numDisplay = '';
 let firstNumber = '';
 let secondNumber = '';
+let output = '';
 let operator = '';
 let numberFocus = 1;
 
@@ -19,6 +20,39 @@ const numBtns = document.querySelectorAll(".number")
 // detect display
 const display = document.querySelector('h1');
 
+// operator function 
+function operate (operator,firstNumber,secondNumber) {
+    
+    if (operator === "+") {
+        output = add(firstNumber,secondNumber);
+        truncate()
+    } else if (operator === "-") {
+        output = subtract(firstNumber,secondNumber);
+        truncate()
+    } else if (operator === "*") {
+        output = multiply(firstNumber,secondNumber);
+        truncate()
+    } else if (operator === "/") {
+        if (secondNumber === 0) {
+            displayValue('nice try');
+        } else {
+            output = divide(firstNumber,secondNumber);
+            truncate()
+        }
+    }
+    numberFocus = 2;
+    numDisplay = '';
+}
+
+// truncate display if necessary
+function truncate (number) {
+    if (output > 9999999) {
+        displayValue("too big");
+    } else {
+        displayValue(parseFloat((output).toFixed(4)));   
+    }
+}
+
 // basic calculator functions
 
     // add
@@ -33,35 +67,21 @@ const display = document.querySelector('h1');
     // divide 
     divide = (firstNumber,secondNumber) => firstNumber / secondNumber;
 
-// operator function 
-function operate (operator,firstNumber,secondNumber) {
-    if (operator === "+") {
-        firstNumber = add(firstNumber,secondNumber);
-    } else if (operator === "-") {
-        firstNumber = subtract(firstNumber,secondNumber);
-    } else if (operator === "*") {
-        firstNumber = multiply(firstNumber,secondNumber);
-    } else if (operator === "/") {
-        firstNumber = divide(firstNumber,secondNumber);
-    }
-    numberFocus = 1;
-    displayValue(firstNumber);   
-}
 
 // add values to number variables
-function addToFirstNumber (number) {
-    numDisplay = numDisplay.concat(number);
-    displayValue(numDisplay);
-    firstNumber = parseFloat(numDisplay);
-    console.log("First number is " + firstNumber);
-}
+    function addToFirstNumber (number) {
+        numDisplay = numDisplay.concat(number);
+        displayValue(numDisplay);
+        firstNumber = parseFloat(numDisplay);
+        console.log("First number is " + firstNumber);
+    }
 
-function addToSecondNumber (number) {
-    numDisplay = numDisplay.concat(number);
-    displayValue(numDisplay);
-    secondNumber = parseFloat(numDisplay);
-    console.log("Second number is " + secondNumber);
-}
+    function addToSecondNumber (number) {
+        numDisplay = numDisplay.concat(number);
+        displayValue(numDisplay);
+        secondNumber = parseFloat(numDisplay);
+        console.log("Second number is " + secondNumber);
+    }
 
 // clear entered numbers on display
 function clearBtn () {
@@ -72,6 +92,7 @@ function clearBtn () {
     } else {
         firstNumber = '';
         secondNumber = '';
+        numberFocus = 1;
     }
     
 }
@@ -82,37 +103,40 @@ function displayValue(n) {
 }
 
 // function buttons
-addbtn.addEventListener('click', () => {
-    numDisplay = '';
-    operator = "+";
-    numberFocus = 2;
-    console.log(operator)
-});
+    addbtn.addEventListener('click', () => {
+        numDisplay = '';
+        operator = "+";
+        numberFocus = 2;
+        console.log(operator)
+    });
 
-subtractbtn.addEventListener('click', () => {
-    numDisplay = '';
-    operator = "-";
-    numberFocus = 2;
-    console.log(operator)
-});
+    subtractbtn.addEventListener('click', () => {
+        numDisplay = '';
+        operator = "-";
+        numberFocus = 2;
+        console.log(operator)
+    });
 
-multiplybtn.addEventListener('click', () => {
-    numDisplay = '';
-    operator = "*";
-    numberFocus = 2;
-    console.log(operator)
-});
+    multiplybtn.addEventListener('click', () => {
+        numDisplay = '';
+        operator = "*";
+        numberFocus = 2;
+        console.log(operator)
+    });
 
-dividebtn.addEventListener('click', () => {
-    numDisplay = '';
-    operator = "/";
-    numberFocus = 2;
-    console.log(operator)
-});
+    dividebtn.addEventListener('click', () => {
+        numDisplay = '';
+        operator = "/";
+        numberFocus = 2;
+        console.log(operator)
+    });
 
-operatebtn.addEventListener('click', () => operate(operator,firstNumber,secondNumber));
+    operatebtn.addEventListener('click', () => {
+        operate(operator,firstNumber,secondNumber)
+        firstNumber = output;
+    });
 
-clearbtn.addEventListener('click', () => clearBtn());
+    clearbtn.addEventListener('click', () => clearBtn());
 
 // retrieve numbers from number buttons
 numBtns.forEach((button) => {
@@ -121,7 +145,7 @@ numBtns.forEach((button) => {
             addToFirstNumber(button.id);
         }   else {
             addToSecondNumber(button.id);
-        }
-        
+        }   
     });
   });
+
